70775981de96ad9a84c346c5aed6f176cff66098
mbiken <25502165+bikenmoirangthem@users.noreply.github.com>
2020-01-08 22:52:57 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -6,11 +6,11 @@ def find_factorial(x):
     if n<0:
         print('Factorial not exists for negetive number')
     elif n==0:
-        print('Factorial of {} is {}'.format(n,1))
+        print('Factorial of {} is {}'.format(n,factorial))
     else:
         for i in range(1,n+1):
             factorial = factorial*i
-        print('Factorial of {}'.format(factorial))
+        print('Factorial of {} is {}'.format(n,factorial))
 
 #getting input from user
 num = int(input('Enter the number you want to find factorial : '))
